29bc315117c678222cc0400be67fc900b1408a85
cj-wang <changjiang.wang@optus.com.au>
2018-10-04 11:32:02 +1000

    feat(nest): serve ng app in public folder

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as path from 'path';
+import * as express from 'express';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(express.static(path.join(__dirname, 'public')));
   await app.listen(3000);
 }
 bootstrap();
