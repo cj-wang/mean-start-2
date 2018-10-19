735b0f1439f1fd007bb886e8465bb40f63374ce1
cj-wang <changjiang.wang@optus.com.au>
2018-10-19 14:04:58 +1100

    feat(nest): set global prefix /api

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Get, Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('api')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
