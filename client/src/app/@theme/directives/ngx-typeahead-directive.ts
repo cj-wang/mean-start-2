7a362fac251cd46ea8ae6a1436454e1136cefa2d
cj-wang <changjiang.wang@optus.com.au>
2018-12-31 15:44:03 +1100

    feat(typeahead): add valueField and labelField

--- client/src/app/@theme/directives/ngx-typeahead-directive.ts
+++ client/src/app/@theme/directives/ngx-typeahead-directive.ts
@@ -2,7 +2,7 @@ import { Directive, OnInit, Input, Component, ViewContainerRef, ComponentFactory
   Injector, NgZone, forwardRef } from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { Observable, of } from 'rxjs';
-import { debounceTime, distinctUntilChanged, switchMap, tap, catchError } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, switchMap, tap, map, catchError } from 'rxjs/operators';
 import { NgbTypeahead, NgbTypeaheadConfig } from '@ng-bootstrap/ng-bootstrap';
 import { Live } from '@ng-bootstrap/ng-bootstrap/util/accessibility/live';
 import { Live as _Live } from './ng-bootstrap/util/accessibility/live';
@@ -50,6 +50,8 @@ export class NgxTypeaheadDirective extends NgbTypeahead implements OnInit {
 
   private iconComponent: NgxTypeaheadIconComponent;
 
+  private queryResult: any = {};
+
   constructor(
       private el: ElementRef,
       private viewContainerRef: ViewContainerRef,
@@ -63,6 +65,12 @@ export class NgxTypeaheadDirective extends NgbTypeahead implements OnInit {
     super(el, viewContainerRef, renderer, injector, componentFactoryResolver, config, ngZone, live as any as Live);
   }
 
+  @Input() valueField: string;
+
+  @Input() set labelField(labelField: string) {
+    this.resultFormatter = this.inputFormatter = value => value[labelField];
+  }
+
   @Input() set ngxTypeahead(query: (term: string) => Observable<any[]>) {
     this.ngbTypeahead = (text$: Observable<string>) => text$.pipe(
       debounceTime(300),
@@ -73,7 +81,12 @@ export class NgxTypeaheadDirective extends NgbTypeahead implements OnInit {
       }),
       switchMap(term => (term.length < 2 ? of([]) : query(term)).pipe(
         tap(() => this.iconComponent.loading = false),
-        catchError((err) => {
+        // extract value by this.valueField if provided
+        map(queryResult => this.valueField ? queryResult.map(item => {
+          this.queryResult[item[this.valueField]] = item;
+          return item[this.valueField];
+        }) : queryResult),
+        catchError(err => {
           this.notificationService.clear();
           this.iconComponent.error = err.error.message || err.error.errorMessage || 'Error';
           return of([]);
@@ -81,6 +94,8 @@ export class NgxTypeaheadDirective extends NgbTypeahead implements OnInit {
       )),
     );
   }
+
+  @Input() getByValue: (value: string) => Observable<any>;
 
   ngOnInit() {
     const inputEl = this.el.nativeElement;
@@ -97,6 +112,39 @@ export class NgxTypeaheadDirective extends NgbTypeahead implements OnInit {
     setTimeout(() => {
       this.renderer.insertBefore(inputEl.parentNode, iconEl, inputEl);
     }, 0);
+
+    // need to get the value object if this.valueField is used
+    if (this.valueField) {
+      if (this.resultFormatter) {
+        const _resultFormatter = this.resultFormatter;
+        this.resultFormatter = value => _resultFormatter(this.queryResult[value]);
+      }
+      if (this.inputFormatter) {
+        const _inputFormatter = this.inputFormatter;
+        this.inputFormatter = value => {
+          if (this.queryResult[value]) {
+            return _inputFormatter(this.queryResult[value]);
+          } else {
+            this.iconComponent.error = null;
+            this.iconComponent.loading = true;
+            if (this.getByValue) {
+              this.getByValue(value).subscribe(valueObject => {
+                this.iconComponent.loading = false;
+                this.queryResult[value] = valueObject;
+                super.writeValue(value);
+              }, err => {
+                this.notificationService.clear();
+                this.iconComponent.error = err.error.message || err.error.errorMessage || 'Error';
+              });
+              return '...';
+            } else {
+              this.iconComponent.error = 'Error';
+              throw new Error('getByValue function not specified for ngxTypeahead');
+            }
+          }
+        };
+      }
+    }
 
     super.ngOnInit();
   }
