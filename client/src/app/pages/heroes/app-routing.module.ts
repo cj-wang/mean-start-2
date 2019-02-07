34923b4d21bbc5170da8356554e148252bb811ce
cj-wang <changjiang.wang@optus.com.au>
2019-02-07 14:51:52 +1100

    refactor: convert heroes RouterModule to sub module

--- client/src/app/pages/heroes/app-routing.module.ts
+++ client/src/app/pages/heroes/app-routing.module.ts
@@ -1,19 +1,23 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AppComponent } from './app.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
 import { HeroesComponent }      from './heroes/heroes.component';
 import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'heroes', component: HeroesComponent }
-];
+const routes: Routes = [{
+  path: '',
+  component: AppComponent,
+  children: [
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'detail/:id', component: HeroDetailComponent },
+    { path: 'heroes', component: HeroesComponent }
+  ]
+}];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
+  imports: [ RouterModule.forChild(routes) ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule {}
