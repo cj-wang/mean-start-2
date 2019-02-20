7a4ae3a8b42950d8ba98a7849488d33736e48a8f
cj-wang <changjiang.wang@optus.com.au>
2019-02-21 10:11:45 +1100

    feat: update title and footer text

--- client/src/app/@theme/components/footer/footer.component.ts
+++ client/src/app/@theme/components/footer/footer.component.ts
@@ -4,7 +4,10 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">Created with ♥ by <b><a href="https://akveo.com" target="_blank">Akveo</a></b> 2017</span>
+    <span class="created-by">
+      <i class="ion ion-social-github mr-1"></i>
+      <b><a href="https://github.com/cj-wang/mean-start-2" target="_blank">GitHub</a></b>
+    </span>
     <div class="socials">
       <a href="#" target="_blank" class="ion ion-social-github"></a>
       <a href="#" target="_blank" class="ion ion-social-facebook"></a>
