330f2096fd4cf681296f915303fee00dc9966bbb
cj-wang <changjiang.wang@optus.com.au>
2019-05-20 15:52:55 +1000

    feat: change toast position BOTTOM_END

--- client/src/app/@theme/services/notification.service.ts
+++ client/src/app/@theme/services/notification.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { NbToastrService } from '@nebular/theme';
+import { NbToastrService, NbGlobalLogicalPosition } from '@nebular/theme';
 
 @Injectable({
   providedIn: 'root',
@@ -11,18 +11,21 @@ export class NotificationService {
   error(message: string, title = 'Error', duration = 10000) {
     return this.toastrService.danger(message, title, {
       duration,
+      position: NbGlobalLogicalPosition.BOTTOM_END,
     });
   }
 
   info(message: string, title = 'Info', duration = 3000) {
     return this.toastrService.info(message, title, {
       duration,
+      position: NbGlobalLogicalPosition.BOTTOM_END,
     });
   }
 
   wait(message: string, title = 'Processing', duration = 0) {
     return this.toastrService.primary(message, title, {
       duration,
+      position: NbGlobalLogicalPosition.BOTTOM_END,
       destroyByClick: false,
       icon: 'nb-loop',
     });
@@ -31,12 +34,14 @@ export class NotificationService {
   success(message: string, title = 'Success', duration = 3000) {
     return this.toastrService.success(message, title, {
       duration,
+      position: NbGlobalLogicalPosition.BOTTOM_END,
     });
   }
 
   warning(message: string, title = 'Warning', duration = 10000) {
     return this.toastrService.warning(message, title, {
       duration,
+      position: NbGlobalLogicalPosition.BOTTOM_END,
     });
   }
 
