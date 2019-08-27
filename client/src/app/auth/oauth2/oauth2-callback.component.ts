628651a95a608ff93d0c9017260009c2e42e031e
cj-wang <Changjiang.Wang@det.nsw.edu.au>
2019-08-27 10:47:38 +1000

    fix: don't set token and redirect on server side

--- client/src/app/auth/oauth2/oauth2-callback.component.ts
+++ client/src/app/auth/oauth2/oauth2-callback.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NbOAuth2AuthStrategy, NbTokenService } from '@nebular/auth';
 
@@ -10,6 +11,7 @@ import { NbOAuth2AuthStrategy, NbTokenService } from '@nebular/auth';
 export class NgxOAuth2CallbackComponent implements OnInit {
 
   constructor(
+    @Inject(PLATFORM_ID) private platform: Object,
     protected router: Router,
     protected activatedRoute: ActivatedRoute,
     protected oauth2AuthStrategy: NbOAuth2AuthStrategy,
@@ -21,11 +23,13 @@ export class NgxOAuth2CallbackComponent implements OnInit {
     // OAuth2 authorization code grant type is done by Passport from Nest server
     // The token passed from the server is not an OAuth2 token, it's a JWT token generated based on the OAuth2 logged in user
     // Here only need to save the JWT token and redirect to the home page
-    const accessToken = this.activatedRoute.snapshot.queryParams.accessToken;
-    const token = this.oauth2AuthStrategy.createToken(accessToken, true);
-    this.tokenService.set(token);
-    const redirectUrl = this.oauth2AuthStrategy.getOption('token.redirectUri');
-    this.router.navigateByUrl(redirectUrl);
+    if (isPlatformBrowser(this.platform)) {
+      const accessToken = this.activatedRoute.snapshot.queryParams.accessToken;
+      const token = this.oauth2AuthStrategy.createToken(accessToken, true);
+      this.tokenService.set(token);
+      const redirectUrl = this.oauth2AuthStrategy.getOption('token.redirectUri');
+      this.router.navigateByUrl(redirectUrl);
+    }
   }
 
 }
