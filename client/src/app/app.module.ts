e48797927b2f2fbce1e64ac99139ba9346cd6f67
cj-wang <Changjiang.Wang@det.nsw.edu.au>
2019-08-28 15:40:26 +1000

    fix: FOUC caused by preboot

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,9 +3,10 @@
  * Copyright Akveo. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, ɵgetDOM } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser, DOCUMENT } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { PrebootModule } from 'preboot';
 import { CoreModule } from './@core/core.module';
@@ -49,6 +50,32 @@ import {
     NgxCommonModule.forRoot(),
   ],
   bootstrap: [AppComponent],
+  providers: [
+    // To prevent FOUC caused by preboot
+    // https://github.com/angular/preboot/issues/75#issuecomment-421266570
+    {
+      provide: APP_INITIALIZER,
+      useFactory: function(document: HTMLDocument, platformId: Object): Function {
+        return () => {
+          if (isPlatformBrowser(platformId)) {
+            const dom = ɵgetDOM();
+            const styles: any[] = Array.prototype.slice.apply(dom.querySelectorAll(document, `style[ng-transition]`));
+            styles.forEach(el => {
+              // Remove ng-transition attribute to prevent Angular appInitializerFactory
+              // to remove server styles before preboot complete
+              el.removeAttribute('ng-transition');
+            });
+            document.addEventListener('PrebootComplete', () => {
+              // After preboot complete, remove the server scripts
+              setTimeout(() => styles.forEach(el => dom.remove(el)));
+            });
+          }
+        };
+      },
+      deps: [DOCUMENT, PLATFORM_ID],
+      multi: true,
+    },
+  ],
 })
 export class AppModule {
 }
