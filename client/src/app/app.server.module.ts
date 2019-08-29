bb53cf892732a23831270a43a827462497d6fa45
cj-wang <Changjiang.Wang@det.nsw.edu.au>
2019-08-29 16:43:27 +1000

    fix: use absolute URLs for ssr requests

--- client/src/app/app.server.module.ts
+++ client/src/app/app.server.module.ts
@@ -1,9 +1,11 @@
-import {NgModule} from '@angular/core';
-import {ServerModule} from '@angular/platform-server';
-import {ModuleMapLoaderModule} from '@nguniversal/module-map-ngfactory-loader';
+import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ServerModule } from '@angular/platform-server';
+import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
-import {AppModule} from './app.module';
-import {AppComponent} from './app.component';
+import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
+import { UniversalInterceptor } from './universal-interceptor';
 
 @NgModule({
   imports: [
@@ -12,5 +14,10 @@ import {AppComponent} from './app.component';
     ModuleMapLoaderModule,
   ],
   bootstrap: [AppComponent],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: UniversalInterceptor,
+    multi: true,
+  }],
 })
 export class AppServerModule {}
