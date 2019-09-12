b6a92d8c3feabbc9e4d9a5af49640ce422d87e3c
cj-wang <Changjiang.Wang@det.nsw.edu.au>
2019-09-13 09:53:31 +1000

    test: HelloService

--- client/src/app/pages/services/hello.service.spec.ts
+++ client/src/app/pages/services/hello.service.spec.ts
@@ -1,17 +1,33 @@
-import { TestBed, inject } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { HelloService } from './hello.service';
 
 describe('HelloService', () => {
+  let service: HelloService;
+  let httpTestingController: HttpTestingController;
+  
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule],
+      imports: [ HttpClientTestingModule ],
       providers: [HelloService],
     });
+    service = TestBed.get(HelloService);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
-  it('should be created', inject([HelloService], (service: HelloService) => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
-  }));
+  });
+
+  it('#hello should call GET api/hello', () => {
+    const greeting = 'Hello';
+    service.hello().then(result => {
+      expect(result).toBe(greeting);
+    });
+    const request = httpTestingController.expectOne('api/hello');
+    expect(request.request.method).toBe('GET');
+    request.flush({ greeting });
+    httpTestingController.verify();
+  });
 });
