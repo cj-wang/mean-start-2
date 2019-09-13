d00b623351dbf8313ef01f2992da28eb6aba94f6
cj-wang <Changjiang.Wang@det.nsw.edu.au>
2019-09-13 13:25:00 +1000

    test: add testing tools from ng testing example project
    https://stackblitz.com/angular/dnboqdpmggpr

---
+++ client/src/testing/jasmine-matchers.d.ts
@@ -0,0 +1,12 @@
+declare namespace jasmine {
+  interface Matchers<T> {
+    toHaveText(actual: any, expectationFailOutput?: any): jasmine.CustomMatcher;
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
